97acccb531e5f9a211427770a422daa0d4320076
Gongqi Huang <gongqih@hotmail.com>
2024-06-14 13:41:15 +0800

    Update nix-shell to use a newer nixpkgs channel

--- shell.nix
+++ shell.nix
@@ -1,4 +1,11 @@
-{ pkgs ? import <nixpkgs> {} }:
+let
+  nixpkgsSrc = builtins.fetchGit {
+    url = "https://github.com/NixOS/nixpkgs";
+    ref = "nixos-24.05";
+    rev = "3c80acabe4eef35d8662733c7e058907fa33a65d";
+  };
+  pkgs = import nixpkgsSrc {};
+in
 
 with pkgs;
 
