1198700b4362cbdb3ad81ddd22d692811002dce1
emilyz1 <emily.rn.zhang@gmail.com>
2024-07-24 19:34:22 -0700

    remove panic

--- fuse-test/mount/build.rs
+++ fuse-test/mount/build.rs
@@ -1,7 +1,7 @@
 use std::io::Result;
 
 fn main() -> Result<()> {
-    panic!("sjiewog");
+    //panic!("sjiewog");
     prost_build::compile_protos(&["src/syscalls.proto"], &["src/"])?;
     Ok(())
 }
